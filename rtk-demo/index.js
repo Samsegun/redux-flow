25c6f1687a7b3fa098029278a9a46750c9d405e9
Samsegun <oyebadesamsegun@gmail.com>
2024-01-12 22:27:45 +0100

    async-thunk flow

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,4 +1,5 @@
 const store = require("./app/store");
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const icecreamActions =
     require("./features/icecream/icecreamSlice").icecreamActions;
@@ -9,13 +10,17 @@ const unsubscribe = store.subscribe(() => {
     console.log("updated state ", store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(icecreamActions.restocked(2));
+store.dispatch(fetchUsers());
 
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(cakeActions.restocked(3));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
 
-unsubscribe();
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(2));
+
+// Note: when using async-thunks,do not unsubscribe from store cos
+// as the name implies, aysnc-thunks are asynchronous.
+// unsubscribe();
