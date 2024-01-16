e37723ad226df152a11a59fc4e26736073f26312
Samsegun <oyebadesamsegun@gmail.com>
2024-01-16 15:33:18 +0100

    user view implemented

--- react-rtk-demo/src/features/user/UserView.jsx
+++ react-rtk-demo/src/features/user/UserView.jsx
@@ -1,7 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "./userSlice";
+
 const UserView = () => {
+    const user = useSelector(state => state.user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, [dispatch]);
+
+    if (user.loading) {
+        return <h2>Loading users....</h2>;
+    }
+
+    if (user.error) {
+        return <h2>{user.error}</h2>;
+    }
+
     return (
         <div>
             <h2>List of users</h2>
+            <ul>
+                {user.users.map(user => {
+                    return <li key={user.id}>{user.name}</li>;
+                })}
+            </ul>
         </div>
     );
 };
